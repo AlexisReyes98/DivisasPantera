3daa332e720399f9a1ba856306caff64ff9a14f8
AlexisReyes98 <giovanny29.garv@gmail.com>
2021-06-13 18:57:44 -0500

    Agregando server

--- DivPant/src/app/model/autenticacion.ts
+++ DivPant/src/app/model/autenticacion.ts
@@ -1,2 +1,6 @@
 export class Autenticacion {
+    Num_Empleado?: string;  //El simbolo '?' es para la parte del front
+    contra_E?: string;
+    Num_Cuenta?: string;
+    Contra_S?: string;    
 }
