82d9c80d1dc6297b47e02a0ff09352bc3e92151f
AlexisReyes98 <giovanny29.garv@gmail.com>
2021-06-14 20:08:57 -0500

    Refinando componentes

--- DivPant/src/app/cuenta-bancaria/cuenta-bancaria.module.ts
+++ DivPant/src/app/cuenta-bancaria/cuenta-bancaria.module.ts
@@ -1,24 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CuentaBancComponent } from './cuenta-banc/cuenta-banc.component';
 import { InfoPersonalComponent } from './info-personal/info-personal.component';
 import { EstadoDeCuentaComponent } from './estado-de-cuenta/estado-de-cuenta.component';
 import { HistPagosComponent } from './hist-pagos/hist-pagos.component';
 import { HistComprasComponent } from './hist-compras/hist-compras.component';
 import { HistTransferenciasComponent } from './hist-transferencias/hist-transferencias.component';
-import { SaldosCargadosComponent } from './saldos-cargados/saldos-cargados.component';
-
+import { HistSaldosCargadosComponent } from './hist-saldos-cargados/hist-saldos-cargados.component';
 
 
 @NgModule({
   declarations: [
-    CuentaBancComponent,
     InfoPersonalComponent,
     EstadoDeCuentaComponent,
     HistPagosComponent,
     HistComprasComponent,
     HistTransferenciasComponent,
-    SaldosCargadosComponent
+    HistSaldosCargadosComponent,
   ],
   imports: [
     CommonModule
