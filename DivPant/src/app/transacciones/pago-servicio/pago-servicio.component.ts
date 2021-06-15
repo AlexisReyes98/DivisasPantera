2f5e837e2cf24990e6d4284452904c1195fff5df
AlexisReyes98 <giovanny29.garv@gmail.com>
2021-06-15 03:52:22 -0500

    Talvez el bueno

--- DivPant/src/app/transacciones/pago-servicio/pago-servicio.component.ts
+++ DivPant/src/app/transacciones/pago-servicio/pago-servicio.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pago-servicio.component.css']
 })
 export class PagoServicioComponent implements OnInit {
+  entrar3:boolean = true;
+  salir3:boolean = false;
+
+  lista = document.getElementById("lista");
+	tareaInput = document.getElementById("tareaInput");
+	btnNuevaTarea = document.getElementById("btn-agregar");
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  next() {
+    this.entrar3 = false;
+    this.salir3 = true;
   }
 
 }
